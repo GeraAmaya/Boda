cf117c5b92d7d4a8f5aec73bcadb93a3f5f71e51
Gera Amaya <gerardoamaya.10@gmail.com>
2024-10-01 14:36:15 -0300

    deploy img

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ window.addEventListener('load', () => {
             // Permite mostrar el contenido de la página
             document.body.style.overflow = 'auto'; 
         }, 1000); // Duración de la transición de desvanecimiento
-    }, 7000); // 7 segundos de duración del loader
+    }, 1000); // 7 segundos de duración del loader
 });
 
 
